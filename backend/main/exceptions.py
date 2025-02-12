45fc2d532f36e5f81096729a98e5b056620aefa2
it-smt <smt_razrab@mail.ru>
2025-02-12 21:06:14 +0500

    Добавлены новые исключения

--- backend/main/exceptions.py
+++ backend/main/exceptions.py
@@ -1,21 +1,27 @@
+from abc import ABC
+from http import HTTPStatus
+from logging import Logger, getLogger
+
 from django.http import JsonResponse
 
 from main.api.v1.schemas import SMsg
 from main.models import Order
 
+logger: Logger = getLogger("django")
 
-class HttpException(Exception):
+
+class HttpException(Exception, ABC):
     """Базовый класс для исключений."""
 
     def __init__(self, message: str, status: int) -> None:
-        self.message: str = message
         self.status: int = status
         self.response = JsonResponse(
             SMsg(msg=message).model_dump(),
             status=status,
             safe=False,
         )
-        super().__init__(self.message)
+        logger.warning("HTTP Exception: %s - %s", status, message)
+        super().__init__(message)
 
     def __call__(self) -> JsonResponse:
         return self.response
@@ -25,15 +31,28 @@ class Http400EmptyItems(HttpException):
     """Исключение для пустого списка блюд."""
 
     def __init__(self) -> None:
-        super().__init__("Заказ должен содержать хотя бы одно блюдо.", 400)
+        super().__init__(
+            "Заказ должен содержать хотя бы одно блюдо.", HTTPStatus.BAD_REQUEST
+        )
 
 
 class Http400IncorrectStatus(HttpException):
     """Исключение для некорректного статуса заказа."""
 
+    _ALLOWED_STATUSES: str = ", ".join(map(str, Order.Status.values))
+
     def __init__(self) -> None:
-        statuses: str = ", ".join(Order.Status.values)
         super().__init__(
-            f"Статус может иметь только следующие значения: {statuses}",
-            400,
+            f"Статус может иметь только следующие значения: {self._ALLOWED_STATUSES}",
+            HTTPStatus.BAD_REQUEST,
+        )
+
+
+class Http404ItemsNotFound(HttpException):
+    """Исключение для несуществующего объекта."""
+
+    def __init__(self) -> None:
+        super().__init__(
+            "Некоторые блюда не были найдены.",
+            HTTPStatus.BAD_REQUEST,
         )
